3fdf2143915d7900c7711156d78b6d81b60d012d
ishikaabhatia <ishikaabhatia09@gmail.com>
2026-04-19 12:34:38 +0530

    add the services solution of admin panel

--- frontend/src/modules/user/contexts/UserModuleDataContext.jsx
+++ frontend/src/modules/user/contexts/UserModuleDataContext.jsx
@@ -159,6 +159,7 @@ export const UserModuleDataProvider = ({ children }) => {
         bookingTypeConfig,
         categories,
         services,
+        setServices,
         banners,
         providers,
         officeSettings,
