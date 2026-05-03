4c9af687892ece29f366f6b054872e69b3add05b
sagar chauhan <sagarchouhan7609@mail.com>
2026-05-03 14:33:47 +0530

    firebase config file update

--- backend/src/config/newconfig.js
+++ backend/src/config/newconfig.js
@@ -1,1 +1,10 @@
 // config folder.
+const firebaseConfig = {
+  apiKey: "AIzaSyA5xnsqVwT59RgBWohNY6PatwWjJ5zFgcU",
+  authDomain: "stylingwithmuskan-635f3.firebaseapp.com",
+  projectId: "stylingwithmuskan-635f3",
+  storageBucket: "stylingwithmuskan-635f3.firebasestorage.app",
+  messagingSenderId: "1023356359588",
+  appId: "1:1023356359588:web:a761cf31880d501587a731",
+  measurementId: "G-5QK3HC3KL1"
+};
